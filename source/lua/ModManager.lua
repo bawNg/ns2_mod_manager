4869eb82b4c8d347ae961e57b86f339a038ac39d
bawNg <bawng@intoxicated.co.za>
2014-01-23 20:56:06 +0200

    implemented mod API support for server implemented multi-part mod downloading started implementing mod uploading support

--- source/lua/ModManager.lua
+++ source/lua/ModManager.lua
@@ -1,7 +1,13 @@
 Script.Load("lua/md5.lua")
 
-local kMaxConcurrentModDownloads = 8
-local kMaxModDownloadAttempts = 20
+local kMaxDownloadChunkSize = 5120 * 1024.0
+local kMaxConcurrentChunkDownloads = 2
+local kMaxConcurrentDownloads = 8
+local kMaxDownloadAttempts = 20
+
+local kMaxConcurrentUploads = 8
+local kMaxUploadAttempts = 8
+
 local kServerUserAgent = string.format("NS2 Server: %s:%d", IPAddressToString(Server.GetIpAddress()), Server.GetPort())
 
 local kMaxModDownloadUrlLength = 256
@@ -109,17 +115,26 @@ local waitingForMods = 0
 
 local queuedMods = { }
 local downloadingMods = { }
+
+local queuedModUploads = { }
+local uploadingMods = { }
+
 local modInfo = { }
 
 ModManager = { }
+
+local function DownloadNextQueuedMod()
+    local dirName = table.remove(queuedMods, 1)
+    ModManager.DownloadMod(dirName)
+end
 
 function ModManager.LoadMod(dirName)
     local matchingPaths = { }
     
     Shared.GetMatchingFileNames("config://mods/" .. dirName, false, matchingPaths)
 
-    if #matchingPaths then
-        if Shared.GetDirHash("config://mods/" .. dirName) == modInfo[dirName].hash then                
+    if #matchingPaths > 0 then
+        if Shared.GetDirHash("config://mods/" .. dirName) == modInfo[dirName].hash then
             Shared.Message("[ModManager] Mounting mod directory: " .. dirName)
             
             Shared.MountModDir(dirName)
@@ -128,7 +143,7 @@ function ModManager.LoadMod(dirName)
                 Shared.Message("[ModManager] All mods have been loaded successfully")
                 ModManager.OnLoadingComplete()
             else
-                ModManager.DownloadNextQueuedMod()
+                DownloadNextQueuedMod()
             end
         else
             Shared.Message("[ModManager] Invalid mod directory: " .. dirName)
@@ -140,26 +155,15 @@ function ModManager.LoadMod(dirName)
 end
 
 function ModManager.QueueMod(dirName)
-    if #downloadingMods < kMaxConcurrentModDownloads then
+    if #downloadingMods < kMaxConcurrentDownloads then
         ModManager.DownloadMod(dirName)
     else
-        table.insert(queuedMods, message.dirName)
+        table.insert(queuedMods, dirName)
     end
-end
-
-function ModManager.DownloadNextQueuedMod()
-    if #queuedMods == 0 then
-        return false
-    end
-
-    local dirName = table.remove(queuedMods, 1)
-    ModManager.DownloadMod(dirName)
-
-    return true
 end
 
 function ModManager.DownloadMod(dirName)
-    if modInfo[dirName].attempts >= kMaxModDownloadAttempts then
+    if modInfo[dirName].attempts >= kMaxDownloadAttempts then
         Shared.ModMissingError("Failed to download mod: " .. dirName)
         return
     end
@@ -168,42 +172,107 @@ function ModManager.DownloadMod(dirName)
 
     modInfo[dirName].attempts = modInfo[dirName].attempts + 1
 
-    local modDownloadUrl = string.format("%s/%s.zip", ModManager.downloadUrl, dirName)
+    local zipPath = string.format("config://mods/%s.zip", dirName)
+    local downloadUrl = string.format("%s/%s.zip", ModManager.downloadUrl, dirName)
+    local totalBytes, totalParts, downloadingParts = 0
+    local queuedParts = { }
+    local downloadedParts = { }
 
-    Shared.Message(string.format("[ModManager] Downloading mod: %s (attempt: %d)", modDownloadUrl, modInfo[dirName].attempts))
+    local function WritePartsToDisk()
+        local zipFile = io.open(zipPath, "wb")
+        local startedAt = Shared.GetTime()
 
-    --TODO: download mods in multiple parts using header support, track progress and retry parts on failure instead of entire mod
+        for i = 1, totalParts do
+            zipFile:write(downloadedParts[i])
+        end
+
+        io.close(zipFile)
+
+        Shared.Message(string.format("[ModManager] Mod archive written to disk: %s (took: %.2f)", dirName, Shared.GetTime() - startedAt))
+    end
+
+    local function ExtractMod(dirName)
+        Shared.Message("[ModManager] Extracting mod: " .. dirName)
+
+        local startedAt = Shared.GetTime()
+
+        Shared.ExtractZip(zipPath, "config://mods", function(succeeded)
+
+            if succeeded then
+                Shared.Message(string.format("[ModManager] Mod extraction complete: %s (took: %.2f seconds)", dirName, Shared.GetTime() - startedAt))
+
+                ModManager.LoadMod(dirName)
+            else
+                Shared.Message(string.format("[ModManager] Mod extraction failed: %s (requeuing mod)", dirName))
+
+                ModManager.QueueMod(dirName)
+            end
+
+        end)
+    end
+
+    local function DownloadModPart(partNumber, attempts)
+        attempts = attempts or 1
+
+        if attempts > kMaxDownloadAttempts then
+            Shared.ModMissingError("Failed to download mod: " .. dirName)
+            return
+        end
+
+        downloadingParts = downloadingParts + 1
+
+        Shared.Message(string.format("[ModManager] Downloading mod: %s (part: %d/%d, attempt: %d)", downloadUrl, partNumber, totalParts, attempts))
+
+        local firstByte = partNumber == 1 and 0 or (partNumber - 1) * kMaxDownloadChunkSize + 1
+        local range = string.format("bytes=%d-%d", firstByte, firstByte + kMaxDownloadChunkSize)
+
+        Shared.SendHTTPRequest {
+            url = downloadUrl,
+            header = { ["User-Agent"] = kServerUserAgent, ["Range"] = range },
+
+            onSuccess = function(body)
+                downloadingParts = downloadingParts - 1
+
+                if #queuedParts > 0 then
+                    local nextPart = table.remove(queuedParts, 1)
+                    DownloadModPart(nextPart)
+                elseif downloadingParts == 0 then
+                    table.removevalue(downloadingMods, dirName)
+                    WritePartsToDisk()
+                    ExtractMod()
+                end
+            end,
+
+            onError = function(body, header, code)
+                Shared.Message(string.format("[ModManager] Mod part %d/%d download attempt #%d failed: %s (response code: %d)", partNumber, totalParts, attempts, dirName, code))
+
+                downloadingParts = downloadingParts - 1
+
+                DownloadModPart(partNumber, attempts + 1)
+            end
+        }
+    end
+
+    Shared.Message(string.format("[ModManager] Mod download attempt #%d: %s", modInfo[dirName].attempts, downloadUrl))
+
     Shared.SendHTTPRequest {
-        url = modDownloadUrl,
+        method = "HEAD",
+        url = downloadUrl,
         header = { ["User-Agent"] = kServerUserAgent },
 
-        onSuccess = function(archiveContents)
-            local zipFilePath = string.format("config://mods/%s.zip", dirName)
-            local zipFile = io.open(tarFilePath, "wb")
+        onSuccess = function(body, header, code)
+            totalBytes = tonumber(header['Content-Length'])
+            totalParts = totalBytes / kMaxDownloadChunkSize
 
-            zipFile:write(archiveContents)
+            Shared.Message(string.format("[ModManager] Mod download starting: %s (%d parts, %.2f KB)", dirName, totalParts, totalBytes / 1024.0))
 
-            io.close(zipFile)
-
-            Shared.Message("[ModManager] Extracting mod: " .. dirName)
-
-            local startedAt = Shared.GetTime()
-
-            Shared.ExtractZip(zipFilePath, "config://mods", function(succeeded)
-
-                table.removevalue(downloadingMods, dirName)
-
-                if succeeded then
-                    Shared.Message(string.format("[ModManager] Mod extraction complete: %s (took: %.2f seconds)", dirName, Shared.GetTime() - startedAt))
-
-                    ModManager.LoadMod(dirName)
+            for i = 1, totalParts do
+                if i <= kMaxConcurrentChunkDownloads then
+                    DownloadModPart(i)
                 else
-                    Shared.Message(string.format("[ModManager] Mod extraction failed: %s (requeuing mod)", dirName))
-
-                    ModManager.QueueMod(dirName)
+                    table.insert(queuedParts, i)
                 end
-
-            end)
+            end
         end,
 
         onError = function(body, header, code)
@@ -221,6 +290,57 @@ if Server then
     local mapCycle = MapCycle_GetMapCycle()
 
     downloadUrl = mapCycle.mod_download_url
+
+    local function ArchiveModDir(dirName, isWorkshopMod, callback)
+        local dirPath = string.format("%s/%s", isWorkshopMod and "config://mods" or "config://workshop", dirName)
+        local zipPath = string.format("config://mods/%s.zip", dirName)
+        local startedAt = Shared.GetTime()
+
+        Shared.Message("[ModManager] Creating archive for mod directory: " .. dirName)
+
+        Shared.CreateZip(dirPath, zipPath, function(succeeded)
+
+            if succeeded then
+                Shared.Message(string.format("[ModManager] Created archive for mod directory: %s (took: %.2f seconds)", dirName, Shared.GetTime() - startedAt))
+            else
+                Shared.Message("[ModManager] Failed to create archive for mod directory: " .. dirName)
+            end
+
+            callback(succeeded)
+
+        end)
+    end
+
+    local function UploadNextQueuedMod()
+        local dirName = table.remove(queuedModUploads, 1)
+        ModManager.UploadMod(dirName)
+    end
+
+    function ModManager.UploadMod(dirName)
+        if modInfo[dirName].uploadAttempts >= kMaxUploadAttempts then
+            Shared.Message("Failed to upload mod: " .. dirName)
+            --TODO: stop mod from being loaded since clients will be unable to download it
+            return
+        end
+
+        table.insert(uploadingMods, dirName)
+
+        modInfo[dirName].uploadAttempts = modInfo[dirName].uploadAttempts + 1
+
+        local zipPath = string.format("config://mods/%s.zip", dirName)
+
+        Shared.Message(string.format("[ModManager] Uploading mod: %s (attempt: %d)", dirName, modInfo[dirName].uploadAttempts))
+
+        --TODO: send upload metadata to server, get max upload chunk size and upload each chunk to the web server
+    end
+
+    function ModManager.QueueModUpload(dirName)
+        if #uploadingMods < kMaxConcurrentUploads then
+            ModManager.UploadMod(dirName)
+        else
+            table.insert(queuedModUploads, dirName)
+        end
+    end
 
     if downloadUrl then
         Shared.Message("[ModManager] Custom mod download url: " .. downloadUrl)
@@ -266,7 +386,7 @@ if Server then
                 end
             end
 
-            modInfo[dirName] = { hash = Shared.GetDirHash("config://mods/" .. dirName), name = name, attempts = 0 }
+            modInfo[dirName] = { hash = Shared.GetDirHash("config://mods/" .. dirName), name = name, attempts = 0, uploadAttempts = 0 }
 
             ModManager.LoadMod(dirName)
         end
@@ -300,6 +420,25 @@ if Server then
 
     function ModManager.OnLoadingComplete()
         Server.AcceptClientConnections(true)
+    end
+
+    local GetNumMods = Server.GetNumMods
+    function Server.GetNumMods()
+        return enabled and totalMods or GetNumMods()
+    end
+
+    Server.GetNumActiveMods = Server.GetNumMods
+
+    local GetModId = Server.GetModId
+    function Server.GetModId(i)
+        return enabled and true or GetModId(i)
+    end
+
+    Server.GetActiveModId = Server.GetModId
+
+    local GetModTitle = Server.GetModTitle
+    function Server.GetModTitle(i)
+        return enabled and modInfo[modDirNames[i]] or GetModTitle(i)
     end
 
 elseif Client then
